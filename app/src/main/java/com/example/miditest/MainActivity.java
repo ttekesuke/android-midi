4dba3c1ec7f46fc310d7dd3482224bbfb4812c77
ttekesuke <tekesuke1986@hotmail.com>
2019-07-10 02:58:40 +0900

    修正

--- app/src/main/java/com/example/miditest/MainActivity.java
+++ app/src/main/java/com/example/miditest/MainActivity.java
@@ -10,7 +10,7 @@ import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.Spinner;
 
-import com.github.billthefarmer.mididriver.MidiDriver;
+import org.billthefarmer.mididriver.MidiDriver;
 
 public class MainActivity extends AppCompatActivity implements MidiDriver.OnMidiStartListener,
         View.OnTouchListener, AdapterView.OnItemSelectedListener {
